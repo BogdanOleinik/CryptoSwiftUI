e3d266d9fe0af8fd49ecf8fc4b3eeabec497309f
Bogdan Oleinik <bogdan.oleynik@joy-dev.com>
2022-11-22 12:03:02 +0800

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Богдан Олейник on 22.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
