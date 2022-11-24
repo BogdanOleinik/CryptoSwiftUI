5598fba77689b8d2b74d0fae7bc8662d3ca44087
Bogdan Oleinik <bogdan.oleynik@joy-dev.com>
2022-11-24 12:54:01 +0800

    Fetch images

---
+++ Crypto/Services/CoinImageService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageService.swift
+//  Crypto
+//
+//  Created by Богдан Олейник on 24.11.22.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: Coin
+    
+    init(coin: Coin) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
